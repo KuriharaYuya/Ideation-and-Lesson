5355acf86339f9b1b4f392e189b80e44495ba438
kurihara.sk <kurihara.liqd@gmail.com>
2022-09-22 23:36:59 +0900

    Added dynamic processing to user_index views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def index; end
+  def index
+    @users = User.all
+  end
 
   def create
     @user = User.new(user_params)
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,5 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test 'user index page should displayed successfully' do
     get users_path
     assert_template 'users/index'
-    assert_select 'h1', count: 1
   end
 end
