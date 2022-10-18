e81e451d95aca1f45482c915776c55c3620636bb
kurihara.sk <kurihara.liqd@gmail.com>
2022-10-18 23:22:35 +0900

    build heroku test env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth if Rails.env.test?
   def hello
     render html: 'hello, world!'
   end
   include SessionsHelper
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
 end
