68261e1e46b2b41eb0f6753760272abe8d82189d
kurihara.sk <kurihara.liqd@gmail.com>
2022-10-18 22:55:35 +0900

    fix minor user controller bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      redirect_to users_path
       # redirect_to user_url(@user.id)
     else
       flash.now[:notice] = @user.errors.full_messages
