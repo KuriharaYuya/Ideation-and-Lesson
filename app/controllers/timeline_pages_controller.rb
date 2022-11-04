884ee65a0cfa9847a1d6f148dded4541032c3117
kurihara.sk <109059044+KuriharaYuya@users.noreply.github.com>
2022-11-04 12:07:14 +0900

    fix: behavior of view whe nfeed's loading reach maximally

--- app/controllers/timeline_pages_controller.rb
+++ app/controllers/timeline_pages_controller.rb
@@ -14,9 +14,11 @@ class TimelinePagesController < ApplicationController
     @microposts = []
     start_get ||= 0
     @start_get = start_get
-    Micropost.all.order(posted_at: :desc)[0..(start_get + 20)].each do |micropost|
+    @number_of_load_microposts = 21
+    @number_of_posted_microposts = Micropost.where(posted?: true).all.length
+    Micropost.all.order(posted_at: :desc)[0..(start_get + @number_of_load_microposts)].each do |micropost|
       @i ||= 0
-      break if @i == @start_get + 21 || micropost.nil?
+      break if @i == @start_get + (@number_of_load_microposts + 1) || micropost.nil?
 
       if micropost.posted_at.present?
         @microposts.push(micropost)
--- test/controllers/timeline_pages_controller_test.rb
+++ test/controllers/timeline_pages_controller_test.rb
@@ -1,4 +1,9 @@
-require "test_helper"
+require 'test_helper'
 
 class TimelinePagesControllerTest < ActionDispatch::IntegrationTest
+  test 'showing h5 as reached micropost limits' do
+    number_of_get_posts = 1000
+    get root_path, params: { post_length: number_of_get_posts }
+    assert_select 'h5.no_more_posts_msg', count: 1
+  end
 end
