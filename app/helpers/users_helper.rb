e1dc0147fea5023e5cfa89ecd955d721d2950fcb
kurihara.sk <109059044+KuriharaYuya@users.noreply.github.com>
2022-11-04 18:00:11 +0900

    feat: set user inital profile_img

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def reject_other_user_to_access_user_edit_page
     redirect_to request.fullpath.chomp('/edit') unless current_user == @user
   end
+
+  def profile_initial_img
+    'https://static.vecteezy.com/system/resources/thumbnails/006/017/592/small/ui-profile-icon-vector.jpg'
+  end
 end
