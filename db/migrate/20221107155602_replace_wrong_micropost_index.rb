16bf9db1f50ae78d7cd3a416d4f9f8bbc232ada6
kurihara.sk <109059044+KuriharaYuya@users.noreply.github.com>
2022-11-08 01:05:45 +0900

    test: remove broken migration script

--- db/migrate/20221107155602_replace_wrong_micropost_index.rb
+++ db/migrate/20221107155602_replace_wrong_micropost_index.rb
@@ -1,6 +1,5 @@
 class ReplaceWrongMicropostIndex < ActiveRecord::Migration[7.0]
   def change
-    remove_index :notifications, name: :index_notifications_on_micropostpost_id
     add_index :notifications, :micropost_id
   end
 end
