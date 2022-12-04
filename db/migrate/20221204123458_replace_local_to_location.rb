d93e33c49b84f16806c009140db8a7444a9e4b27
kurihara.sk <109059044+KuriharaYuya@users.noreply.github.com>
2022-12-04 22:01:09 +0900

    hotfix: fix migratio file

--- db/migrate/20221204123458_replace_local_to_location.rb
+++ db/migrate/20221204123458_replace_local_to_location.rb
@@ -1,6 +1,5 @@
 class ReplaceLocalToLocation < ActiveRecord::Migration[7.0]
   def change
-    remove_column :time_cards, :local
     add_column :time_cards, :location_today, :string, limit: 40
     # Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
