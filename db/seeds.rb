e727a7c74a8bf3c4f98150119a45691dbe056be0
kurihara.sk <109059044+KuriharaYuya@users.noreply.github.com>
2022-12-15 22:00:00 +0900

    update: seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,21 @@
 # set yuya's 60 microposts
-unless Rails.env.production?
-  60.times do |n|
-    title = "#{n}個目のテスト"
-    start_datetime = Time.now
-    end_datetime = Time.now
-    if Rails.env.development?
-      user_id = 4
-      lifelog_id = 1
-    end
-    if Rails.env.test?
-      user_id = User.all[-1].id.to_i
-      lifelog_id = User.find(user_id).lifelogs[-1].id
-    end
-    exec_date = Lifelog.find(lifelog_id).log_date
-    posted_at = Time.now - n.minutes
-    Micropost.create!(title:, user_id:, start_datetime:, end_datetime:, lifelog_id:, exec_date:, posted?: true,
-                      posted_at:, engagement_status: '完了', post_type: 'タイムラプス')
+# unless Rails.env.production?
+60.times do |n|
+  title = "#{n}個目のテスト"
+  start_datetime = Time.now
+  end_datetime = Time.now
+  user_id = 1
+  lifelog_id = 1
+  if Rails.env.test?
+    user_id = User.all[-1].id.to_i
+    lifelog_id = User.find(user_id).lifelogs[-1].id
   end
+  exec_date = Lifelog.find(lifelog_id).log_date
+  posted_at = Time.now - n.minutes
+  Micropost.create!(title:, user_id:, start_datetime:, end_datetime:, lifelog_id:, exec_date:, posted?: true,
+                    posted_at:, engagement_status: '完了', post_type: 'タイムラプス')
 end
+# end
 
 120.times do |n|
   name  = "TEST_#{Faker::Name.name}"
